f18a9d688381213dc3e5c763cd76652d9183dbf7
mujahid ul islam <117376282+Mujahid4321@users.noreply.github.com>
2024-04-02 16:11:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
-import os
 from flask import Flask
 from sqlalchemy import create_engine
+import os
 
 app = Flask(__name__)
 
-# Fetch database connection details from environment variable
-db_credentials = os.getenv('DB_CREDENTIALS')
-if db_credentials:
-    db_user, db_password, db_host, db_port, db_name = db_credentials.split(';')
-    db_url = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
-    engine = create_engine(db_url)
-else:
-    raise ValueError("DB_CREDENTIALS environment variable is not set.")
+# Fetch database connection details from environment variables
+db_host = os.getenv('DB_HOST')
+db_port_str = os.getenv('DB_PORT')
+db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+
+# Check if required environment variables are set
+if None in (db_host, db_port_str, db_name, db_user, db_password):
+    raise ValueError("One or more required environment variables are not set.")
+
+# Convert DB_PORT to integer if it's set and valid
+db_port = int(db_port_str) if db_port_str.isdigit() else None
+
+# Check if DB_PORT is None or not a valid integer
+if db_port is None:
+    raise ValueError("DB_PORT environment variable is not set or is not a valid integer.")
+
+# Create a connection string
+db_url = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+
+# Create an engine and connect to the database
+engine = create_engine(db_url)
 
 @app.route('/')
 def test_db_connection():
