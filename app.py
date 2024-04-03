5546779d754a1340839e5b4b917bf5928ec6c4fe
mujahid ul islam <117376282+Mujahid4321@users.noreply.github.com>
2024-04-03 15:32:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+db_url = f'postgresql://{db_user}:{quote_plus(db_password)}@{db_host}:{db_port}/{db_name}'db_url = f'postgresql://{db_user}:{quote_plus(db_password)}@{db_host}:{db_port}/{db_name}'import os
 from flask import Flask, jsonify, request
 from sqlalchemy import create_engine, Column, Integer, String, ARRAY, inspect
 from sqlalchemy.ext.declarative import declarative_base
@@ -26,7 +26,7 @@ db_credentials = os.getenv('DB_CREDENTIALS')
 if db_credentials:
     db_user, db_password, db_host, db_port_str, db_name = db_credentials.split(';')
     db_port = int(db_port_str) if db_port_str and db_port_str.isdigit() else None
-    db_url = f'postgresql://{{{db_user}}}:{quote_plus{{db_password}}}@{{{db_host}}}:{db_port}/{{{db_name}}}'
+    db_url = f'postgresql://{db_user}:{quote_plus(db_password)}@{db_host}:{db_port}/{db_name}'
 
 
     # Create the SQLAlchemy engine and session outside the app context
